86e7d3c043ce98e5ce0c49e4d3e65a75dc11ff51
Steve McDaniel <steve@klumzy.com>
2021-05-27 18:48:27 -0400

    fix struct tags in example

--- example/main.go
+++ example/main.go
@@ -6,11 +6,11 @@ import (
 )
 
 type MySettings struct {
-	EthernetDevice string `default: "eth0" description:"Specify NIC to configure"`
-	IpAddress      string `default: "127.0.0.1" description:"IP Address to listen on"`
-	PortNumber     int    `default: "80" description:"IP Address to listen on"`
-	Username       string `default: "root" description:"Default user"`
-	Shell          string `default: "/bin/bash" description:"Default Shell"`
+	EthernetDevice string `default:"eth0" description:"Specify NIC to configure"`
+	IpAddress      string `default:"127.0.0.1" description:"IP Address to listen on"`
+	PortNumber     int    `default:"80" description:"IP Address to listen on"`
+	Username       string `default:"root" description:"Default user"`
+	Shell          string `default:"/bin/bash" description:"Default Shell"`
 }
 
 var settings *MySettings
